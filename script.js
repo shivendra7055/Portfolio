631d4af84bb5789e3bb14039b7fb2d0e0ba49125
shivendra7055 <139779339+shivendra7055@users.noreply.github.com>
2023-10-18 14:57:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -102,22 +102,22 @@ function changeColor() {
 
 // ======Scrool Revel======
 
-// ScrollReveal({
-//     // reset: true,
-//     distance: '80px',
-//     duration:2000,
-//     delay:200
-// });
+ScrollReveal({
+reset: true,
+distance: '80px',
+duration:2000,
+delay:200
+});
 
-// ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-// ScrollReveal({
-//     // reset: true ,
-//     distance: '80px',
-//     duration: 2000,
-//     delay: 200,
-//  });
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal({
+reset: true ,
+distance: '80px',
+duration: 2000,
+delay: 200,
+});
  
-//  ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-//  ScrollReveal().reveal('.home-img img, .service-box, .input-box', { origin: 'bottom' });
-//  ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
-//  ScrollReveal().reveal('.about-content,.home-content p', { origin: 'right' });
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img img, .service-box, .input-box', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
+ScrollReveal().reveal('.about-content,.home-content p', { origin: 'right' });
